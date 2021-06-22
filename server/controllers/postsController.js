1da07d4b54480b66db6fead33d9cbb2796127c26
jean <jin.ahn.1027@gmail.com>
2021-06-22 12:25:49 -0400

    Client: added components folder with necessary files

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,3 +1,25 @@
-export const getPosts = (req, res) => { 
-    res.send('works!');
+
+import Post from '../models/post.js';
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => { 
+    try {
+        const post = await Post.find();
+                
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const { title, content, creator, files } = req.body;
+    const newPost = new Post({ title, content, creator, files })
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
