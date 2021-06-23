5da8a2e8b86efd80770a67da0375f3eb3238c219
jean <jin.ahn.1027@gmail.com>
2021-06-23 09:33:32 -0400

    Client: added like, veiw delete icon to the post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Card, Col, Row} from 'react-bootstrap';
+import ThumbsUp from '../../../images/ThumbsUp.png'
+import View from '../../../images/view.png'
+import Delete from '../../../images/Delete.ico'
 
 const Post = (props) => {
-    const { title, content, creator, files } =props.post
+    const { title, content, creator, files, likes, views } =props.post
     return (
         <Card>
             <Row>
@@ -16,9 +19,18 @@ const Post = (props) => {
                     <Card.Text>
                     {content}
                     </Card.Text>
+                    
                 </Card.Body>
+                
+
                 </Col>
             </Row>
+            
+            <Card.Footer>
+                <img src={View} width="30" height="30" alt="logo"/>View{views}
+                <img src={ThumbsUp} width="30" height="30" alt="logo"/>Like{likes}
+                <img src={Delete} width="30" height="30" alt="logo"/>Delete
+            </Card.Footer>
         </Card>
     );
 };
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Post from './Post/Post';
 import { useSelector } from 'react-redux';
+import { Spinner } from 'react-bootstrap';
 
 const Posts = () => {
     const posts = useSelector((state) => state.posts);
     return (
+        !posts.length ? 
+        <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+        </Spinner>:
         <div>
             {posts.map(p=><Post key={p._id} post={p}/>)}
         </div>
